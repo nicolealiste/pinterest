906ead7df3d96a5d10fdc907833201ffde325032
nicolealiste <nicole.alistecastillo@gmail.com>
2017-10-12 00:17:38 -0300

    resolucion de conflictos

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,15 @@
-var gulp=require('gulp');
-var concat= require('gulp-concat');
-var uglify= require('gulp-uglify');
-var sass= require('gulp-sass');
-var minifyCSS= require('gulp-minify-css');
-var webserver= require('gulp-webserver');
+/*Dependencias*/
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
 
-/*tareas*/
 gulp.task('script', function(){
-	gulp.src(['node_modules/jquery/dist/jquery.js','assets/js/*.js'])
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'assets/js/*.js'])
 	.pipe(concat('script.js'))
-	//carpeta dist
-	.pipe(gulp.dest('dist/js/'))
+	.pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('style', function(){
@@ -18,17 +17,23 @@ gulp.task('style', function(){
 	.pipe(sass().on('error', sass.logError))
 	.pipe(minifyCSS())
 	.pipe(concat('style.min.css'))
-	.pipe(gulp.dest('dist/css/'))
+	.pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('webserver', function(){
-	gulp.src('../gulpie/')
-		.pipe(webserver({
-		fallback:'index.html',
+	gulp.src('../pinterest/')
+	.pipe(webserver({
+		fallback: 'index.html',
 		livereload: true,
 		directoryListing: false,
-		open: true,
-		}));
+		open: true
+	}));
 });
 
-gulp.task('default', ['script', 'style', 'webserver']);
+/*------ Watch SASS ------*/
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/*.scss',['style']);
+});
+
+//Aqui se le indica a gulp cuales son las tareas que debera eecutar al hacer correr el comando gulp
+gulp.task('default', ['script', 'style', 'webserver', 'watch']);
